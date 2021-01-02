df769512c93211845c45e92d35cb22d50de5bbb8
Brian Appleton <appleton6509@outlook.com>
2021-01-01 19:01:50 -0700

    created logic for pinging

--- Core/Models/Echo.cs
+++ Core/Models/Echo.cs
@@ -5,7 +5,7 @@ namespace Data.Models
     public class Echo
     {
         public Guid Id { get; set; }
-        public DateTime TimeStamp { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
         public int StatusCode { get; set; }
         public int Latency { get; set; }
         public string StatusMessage { get; set; }
--- EchoWorkerService/Worker.cs
+++ EchoWorkerService/Worker.cs
@@ -17,24 +17,68 @@ namespace EchoWorkerService
     {
         private readonly ILogger<Worker> _logger;
         private readonly IServiceScopeFactory _scopeFactory;
-        private PingService _ping;
 
-        public Worker(ILogger<Worker> logger, IServiceScopeFactory scope, PingService ping)
+        /// <summary>
+        /// Number of service workers that will run simultaneously
+        /// </summary>
+        private int NumberOfWorkers { get; set; } = 5;
+
+        public Worker(ILogger<Worker> logger, IServiceScopeFactory scope)
         {
             _logger = logger;
             _scopeFactory = scope;
-            _ping = ping;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                PingHost();
-
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await StartWork();
+                _logger.LogInformation("Worker done at: {time}", DateTimeOffset.Now);
+                await Task.Delay(60000, stoppingToken);
             }
+        }
+
+        private async Task StartWork()
+        {
+            List<Task> tasks = new List<Task>();
+            List<Echo> results = new List<Echo>();
+            CreateTasks(tasks, results);
+            await WaitForProcessResults(tasks,results);
+        }
+
+        private async Task WaitForProcessResults(List<Task> tasks, List<Echo> results)
+        {
+            int taskCount = tasks.Count;
+            while (taskCount > 0)
+            {
+                tasks.RemoveAll(x => x.IsCompleted);
+                _logger.LogInformation("Waiting for tasks to finish, number left: " + taskCount, DateTimeOffset.Now);
+                await Task.Delay(1000);
+                taskCount = tasks.Count;
+            }
+            int resultCount = results.Count;
+            while (resultCount > 0)
+            {
+                _logger.LogInformation("Waiting for results to finish processing, number left: "+ resultCount, DateTimeOffset.Now);
+                AddToDatabase(results);
+                await Task.Delay(1000);
+                resultCount = results.Count;
+            }
+        }
+
+        private void AddToDatabase(List<Echo> results)
+        {
+            if (results.Count <= 0)
+                return;
+
+            using var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<UptimeContext>();
+            Echo[] copy = results.ToArray();
+            dbContext.AddRange(copy);
+            dbContext.SaveChanges();
+            results.RemoveAll(x => copy.Contains(x));
         }
 
         private List<EndPoint> GetEndPoints()
@@ -44,35 +88,46 @@ namespace EchoWorkerService
             return dbContext.EndPoint.ToList();
         }
 
-        private void PingHost()
+        private void CreateTasks(List<Task> tasks, List<Echo> results)
         {
             List<EndPoint> data = GetEndPoints();
-            List<Task> tasks = new List<Task>();
-            List<PingResult> results = new List<PingResult>();
-
+            _logger.LogInformation("Endpoint count: " + data.Count, DateTimeOffset.Now);
             foreach (var ep in data)
             {
-                if (tasks.Count < 6)
-                    CreateTasks(ep,tasks,results);
+                if (Object.Equals(ep, null) || Object.Equals(ep.Ip, null))
+                    continue;
 
-                while(tasks.Count > 5)
-                    RemoveCompletedTasks(tasks);
+                int taskCount = tasks.Count;
+                while (taskCount >= NumberOfWorkers)
+                {
+                    tasks.RemoveAll(x => x.IsCompleted);
+                    taskCount = tasks.Count;
+                }
+                if (tasks.Count < NumberOfWorkers)
+                    tasks.Add(Task.Run(() =>
+                    {
+                        using var scope = _scopeFactory.CreateScope();
+                        var ping = scope.ServiceProvider.GetRequiredService<PingService>();
+                        ping.AddressOrIp = ep.Ip;
+                        results.Add(MapResult(ping.StartPingAsync().Result, ep));
+                    }));
+                AddToDatabase(results);
             }
         }
 
-        private static void RemoveCompletedTasks(List<Task> tasks)
+        private Echo MapResult(PingResult result, EndPoint ep)
         {
-            tasks.ForEach(x => { if (x.IsCompleted) tasks.Remove(x); });
-        }
+            if (Object.Equals(result, null)) return null;
 
-        private void CreateTasks(EndPoint ep,List<Task> tasks, List<PingResult> results)
-        {
-            tasks.Add(Task.Run(() =>
+            Echo echo = new Echo()
             {
-                _ping.AddressOrIp = ep.Ip;
-                PingResult result = _ping.StartPingAsync().Result;
-                results.Add(result);
-            }));
+                EndPointId = ep.Id,
+                Latency = result.Latency,
+                StatusCode = result.StatusCode,
+                StatusMessage = (result.ErrorMessage),
+                TimeStamp = DateTime.Now
+            };
+            return echo;
         }
     }
 }
