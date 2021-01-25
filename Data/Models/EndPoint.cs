8be9ac97db5ee46817b30f96e88c7ed2536a8fbc
Brian Appleton <appleton6509@outlook.com>
2021-01-25 12:19:41 -0700

    add validation for endpoint regex

--- Data/Models/EndPoint.cs
+++ Data/Models/EndPoint.cs
@@ -11,9 +11,9 @@ namespace Data.Models
         [StringLength(30, ErrorMessage = "{0} must be a maximum of {1} characters")]
         public string Description { get; set; }
 
-        [Display(Name = "Site / IP")]
-        [RegularExpression(@"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?|^((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",
-    ErrorMessage = "{0} must be a valid site or ip address")]
+        [Display(Name = "Site")]
+        [RegularExpression(@"^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$",
+    ErrorMessage = "{0} must be a valid web address")]
         [StringLength(60, ErrorMessage = "{0} must be a minimum of {2} characters", MinimumLength = 5)]
         public string Ip { get; set; }
 
